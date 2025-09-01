0e2792920ee2bc2e60e839b99a7be3b1e0bfdba0
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-09-02 00:24:34 +0530

    filter changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,11 +2,31 @@ import RestaurantCard from "./RestaurantCard";
 import { resList } from "../util/mockData";
 
 const Body = () => {
+  let resData = resList;
+  console.log(resData);
+  const filterHighRatingRestaurants = () => {
+    resData = resData?.filter(
+      (restaurant) => restaurant?.info?.avgRating > 4.2
+    );
+    console.log(resData);
+  };
   return (
     <div className="main-body">
-      <div className="search">Search</div>
+      <div className="search">
+        <button
+          onClick={() => {
+            resData = resData?.filter(
+              (restaurant) => restaurant?.info?.avgRating > 4.2
+            );
+            console.log(resData);
+          }}
+          className="filter-button"
+        >
+          Higher Rating Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList?.map((restaurant, idx) => (
+        {resData?.map((restaurant, idx) => (
           <RestaurantCard key={restaurant?.info?.id} resObj={restaurant} />
         ))}
       </div>
