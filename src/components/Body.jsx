cc8f868bfef1e6d0747e213420c6569d5128696e
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-09-06 00:27:46 +0530

    did api call to fetch data and set restaurant card data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,36 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../util/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  console.log("page rendered.");
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
   console.log(useState([]));
 
-  console.log(listOfRestaurants);
   const filterHighRatingRestaurants = () => {
     const filteredRestaurants = listOfRestaurants?.filter(
       (restaurant) => restaurant?.info?.avgRating > 4.2
     );
     setListOfRestaurants(filteredRestaurants);
   };
+
+  const api =
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4343684&lng=78.4224553&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+  const getRestaurantData = async () => {
+    console.log("function called");
+    const data = await fetch(api);
+    const json = await data.json();
+
+    const restaurants =
+      json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setListOfRestaurants(restaurants);
+  };
+  useEffect(() => {
+    console.log("Hello useEffect executed.");
+    getRestaurantData();
+  }, []);
+
   return (
     <div className="main-body">
       <div className="search">
