cb86a9d346897cb4ca5d1fa1a984feb96ac0b478
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-09-11 23:15:04 +0530

    routing

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppLayout from "./src/App";
+import About from "./src/components/About";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Error from "./src/components/Error";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
