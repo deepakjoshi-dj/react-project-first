0bda78ce06638e3126d0fe0e2b0bfc4844eb96f4
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-09-15 22:48:43 +0530

    restaurant menu cards

--- src/components/RestaurantsPage.jsx
+++ src/components/RestaurantsPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { RESTAURANT_API } from "../util/constants";
+import { RESTAURANT_API, RESTAURANT_IMG } from "../util/constants";
+import RestaurantSkelton from "./RestaurantSkelton";
 
 const ItemCategory =
   "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
@@ -10,57 +11,126 @@ const NestedItemCategory =
 const RestaurantsPage = () => {
   const [restaurantData, setRestaurantData] = useState([]);
   const [restaurantItems, setRestaurantItems] = useState([]);
-  const [viewAccordion, setViewAccordion] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getRestaurantData = async (restaurantId = "") => {
-    const response = await fetch(RESTAURANT_API);
-    const json = await response.json();
+    try {
+      setLoading(true);
+      const response = await fetch(RESTAURANT_API);
+      const json = await response.json();
+      debugger;
 
-    let filteredRestaurantItems = json?.data?.cards?.find(
-      (card) => card?.groupedCard?.cardGroupMap?.REGULAR?.cards?.length
-    );
-    filteredRestaurantItems =
-      filteredRestaurantItems?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+      let filteredRestaurantItems = json?.data?.cards?.find(
+        (card) => card?.groupedCard?.cardGroupMap?.REGULAR?.cards?.length
+      );
+      filteredRestaurantItems =
+        filteredRestaurantItems?.groupedCard?.cardGroupMap?.REGULAR?.cards;
 
-    filteredRestaurantItems = filteredRestaurantItems?.filter((item) =>
-      [ItemCategory, NestedItemCategory]?.includes(item?.card?.card?.["@type"])
-    );
+      filteredRestaurantItems = filteredRestaurantItems?.filter((item) =>
+        [ItemCategory, NestedItemCategory]?.includes(
+          item?.card?.card?.["@type"]
+        )
+      );
 
-    setRestaurantItems(filteredRestaurantItems);
-    setRestaurantData(json?.data);
+      setRestaurantItems(filteredRestaurantItems);
+      setRestaurantData(json?.data);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     getRestaurantData();
   }, []);
 
-  const handleAccordion = () => {
-    setViewAccordion((prev) => !prev);
-  };
+  console.log(restaurantItems);
+
+  if (loading) return <RestaurantSkelton />;
 
   return (
     <div>
       <h1>{restaurantData?.cards?.[0]?.card?.card?.text}</h1>
 
-      <div className="accordion-wrapper">
-        <button className="accordion-header" onClick={handleAccordion}>
-          <div className="accordion-header--title">Recommended</div>
-          <div className="accordion-header--action-icon">+/-</div>
-        </button>
-        <div className={`accordion-panel-${viewAccordion ? "view" : "none"}`}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-          reiciendis provident aliquam, pariatur animi soluta accusantium
-          perferendis possimus praesentium vel eligendi repudiandae voluptas
-          quaerat maxime quidem ab modi nam odio minima odit quae. Repudiandae
-          dignissimos alias, iste minima voluptates facere maxime eos ex
-          laudantium earum sed excepturi itaque blanditiis quisquam eaque
-          perspiciatis natus dicta quam dolores aliquid facilis animi deleniti.
-          Voluptas vel, architecto, autem in quam repellat impedit ipsam illum
-          consequatur, quisquam quis ab laborum vero deleniti officia provident
-          alias. Architecto repellendus officia deserunt at perspiciatis
-          deleniti? Ex, vitae aut? At laborum cum obcaecati reprehenderit
-          architecto voluptate, rem iusto minus?
+      <div style={{ padding: "1rem" }}>
+        {restaurantItems?.map((restaurant, index) => {
+          if (restaurant?.card?.card?.["@type"] === ItemCategory) {
+            return (
+              <RestaurantAccordion
+                key={`${restaurant?.card?.card?.title}-${index}`}
+                data={restaurant?.card?.card}
+              />
+            );
+          }
+          if (restaurant?.card?.card?.["@type"] === NestedItemCategory) {
+            return (
+              <div style={{ margin: "2rem 0" }}>
+                <h2>{restaurant?.card?.card?.title}</h2>
+                {restaurant?.card?.card?.categories?.map((restaurant) => (
+                  <RestaurantAccordion
+                    key={`${restaurant?.title}-${index}`}
+                    data={restaurant}
+                  />
+                ))}
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+const RestaurantAccordion = (props) => {
+  const { title, itemCards } = props?.data;
+  const [viewAccordion, setViewAccordion] = useState(false);
+  const handleAccordion = () => {
+    setViewAccordion((prev) => !prev);
+  };
+  return (
+    <div className="accordion-wrapper">
+      <button className="accordion-header" onClick={handleAccordion}>
+        <div className="accordion-header--title">
+          {title} ({itemCards?.length})
         </div>
+        <div className="accordion-header--action-icon">+/-</div>
+      </button>
+      <div className={`accordion-panel-${viewAccordion ? "view" : "none"}`}>
+        {itemCards?.map((item) => (
+          <RestaurantMenuView
+            key={item?.card?.info?.name}
+            menuData={item?.card}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const RestaurantMenuView = (props) => {
+  const { name, price, offerTags, description, imageId } =
+    props?.menuData?.info;
+  return (
+    <div className="restaurant-menu-card">
+      <div>
+        <div>{name}</div>
+        <div>
+          {price / 100} {offerTags?.[0]?.title} {offerTags?.[0]?.subTitle}
+        </div>
+        <div>{description}</div>
+      </div>
+      <div className="restaurant-menu-img-container">
+        {imageId ? (
+          <img
+            height="100%"
+            width="100%"
+            src={RESTAURANT_IMG + imageId}
+            alt={name}
+          />
+        ) : (
+          <div>Add</div>
+        )}
       </div>
     </div>
   );
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,5 +1,6 @@
 export const RESTAURANT_IMG =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+// ("https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/6/28/8f18062e-fd85-4b84-91e7-aad8805fad0e_d5146df7-8be8-43fe-8a70-c9ea9c282b1f.jpg");
 export const LOGO =
   "https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg";
 
@@ -7,4 +8,4 @@ export const HOMEPAGE_RESTAURANT_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4343684&lng=78.4224553&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4343684&lng=78.4224553&restaurantId=509082";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4327426&lng=78.3697399&restaurantId=489739";
