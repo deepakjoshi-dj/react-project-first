950f653e81d8a808bc743d3d0e4a9e6b5676b1ed
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2025-09-25 22:46:03 +0530

    tailwind css initialized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,28 +18,28 @@ const Header = () => {
     // console.log("useEffect Run");
   }, [buttonText]);
   return (
-    <div className="header-container">
-      <div className="header-logo">
+    <div className="flex items-center justify-between p-2 bg-gray-100 shadow-lg">
+      <div className="w-15">
         <img alt="logo" height="100%" width="100%" src={LOGO} />
       </div>
-      <ul className="header-nav-items">
-        <li className="header-nav-item">
+      <ul className="flex gap-2 items-center">
+        <li className="">
           Online Status : {isOnline ? "🟢 Online" : "🔴 Offline"}
         </li>
-        <li className="header-nav-item">
+        <li className=" hover:text-blue-700">
           <Link to="/">Home</Link>
         </li>
-        <li className="header-nav-item">
+        <li className=" hover:text-blue-700">
           <Link to="/about"> About Us</Link>
         </li>
-        <li className="header-nav-item">
+        <li className=" hover:text-blue-700">
           <Link to="/contact">Contact Us</Link>
         </li>
-        <li className="header-nav-item">
+        <li className=" hover:text-blue-700">
           <Link to="/grocery">Instamart</Link>
         </li>
-        <li className="header-nav-item">Cart</li>
-        <li>
+        <li className=" hover:text-blue-700">Cart</li>
+        <li className=" hover:text-blue-700">
           <button
             onClick={handleButtonClick}
             style={{ padding: "1rem", cursor: "pointer" }}
--- src/util/constants.js
+++ src/util/constants.js
@@ -2,7 +2,7 @@ export const RESTAURANT_IMG =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 // ("https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/6/28/8f18062e-fd85-4b84-91e7-aad8805fad0e_d5146df7-8be8-43fe-8a70-c9ea9c282b1f.jpg");
 export const LOGO =
-  "https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg";
+  "https://www.reshot.com/preview-assets/icons/KME7TXCDG4/food-addiction-KME7TXCDG4.svg";
 
 export const HOMEPAGE_RESTAURANT_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4343684&lng=78.4224553&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
